d39a7491c2d0b9844835baf749bed72f0a31d53f
Sema-qq <PO-2-06_024@mail.ru>
2017-07-22 16:35:58 +0500

    add project

---
+++ index.php
@@ -0,0 +1,159 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bootstrap</title>
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/font-awesome.min.css">
+    <link rel="stylesheet" href="css/main.css">
+</head>
+<body>
+    <div class="navbar navbar-inverse navbar-fixed-top">
+        <div class="container">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="#">PR<i class="fa fa-circle"></i>GER</a>
+            </div>
+            <div class="navbar-collapse collapse">
+                <ul class="nav navbar-nav navbar-right">
+                    <li class="active"><a href="#">Домой</a></li>
+                    <li><a href="#">Про нас</a></li>
+                    <li><a href="#">Сервисы</a></li>
+                    <li><a href="#">Работы</a></li>
+                    <li><a href="#"><i class="fa fa-envelope-o" aria-hidden="true"></i></a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+
+    <div id="headerwrap">
+        <div class="container">
+            <div class="row centered">
+                <div class="col-lg-8 col-lg-offset-2">
+                    <h1>Bootstrap is awesome!</h1>
+                    <h2>Сайты на Bootstrap отличные</h2>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="container w">
+        <div class="row centered">
+            <br><br>
+            <div class="col-lg-4">
+                <i class="fa fa-heart"></i>
+                <h4>Дизайн</h4>
+                <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+            </div>
+            <div class="col-lg-4">
+                <i class="fa fa-laptop"></i>
+                <h4>Компьютеры</h4>
+                <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+            </div>
+            <div class="col-lg-4">
+                <i class="fa fa-trophy"></i>
+                <h4>Награды</h4>
+                <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+            </div>
+        </div>
+    </div>
+
+    <div id="dg">
+        <div class="container">
+            <div class="row centered">
+                <h4>Последние работы</h4>
+                <br>
+                <div class="col-lg-4">
+                    <div class="tilt">
+                        <a href="#"><img src="img/p01.jpg"></a>
+                    </div>
+                </div>
+                <div class="col-lg-4">
+                    <div class="tilt">
+                        <a href="#"><img src="img/p01.jpg"></a>
+                    </div>
+                </div>
+                <div class="col-lg-4">
+                    <div class="tilt">
+                        <a href="#"><img src="img/p01.jpg"></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="container wb">
+        <div class="row centered">
+            <br><br>
+            <div class="col-lg-8 col-lg-offset-2">
+                <h4>Мы создаем сайты</h4>
+                <p>
+                    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+                </p>
+                <p><br><br></p>
+            </div>
+            <div class="col lg-2"></div>
+            <div class="col lg-10 col-lg-offset-1">
+                <img src="img/munter.jpg" alt="" class="img-responsive">
+            </div>
+        </div>
+    </div>
+
+    <div id="lg">
+        <div class="container">
+            <div class="row centered">
+                <h4>Партнеры</h4>
+                <div class="col-lg-2 col-lg-offset-1">
+                    <img src="img/c01.png" alt="">
+                </div>
+                <div class="col-lg-2">
+                    <img src="img/c02.png" alt="">
+                </div>
+                <div class="col-lg-2">
+                    <img src="img/c03.png" alt="">
+                </div>
+                <div class="col-lg-2">
+                    <img src="img/c04.png" alt="">
+                </div>
+                <div class="col-lg-2">
+                    <img src="img/c05.png" alt="">
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div id="red">
+        <div class="container">
+            <div class="row centered">
+                <div class="col-lg-8 col-lg-offset-2">
+                    <h4>Разработка сайтов на профессиональном уровне</h4>
+                    <p>Если вы регулярно пользуетесь этим сайтом и хотите быть уверенным в его дальнейшем постоянном функционировании, подумайте о небольшом пожертвовании, которое помогло бы оплатить его хостинг и трафик. Нет никаких минимальных сумм - любое пожертвование принимается с благодарностью. Вы можете щёлкнуть здесь чтобы перевести деньги через PayPal. Спасибо за вашу поддержку.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div id="footer">
+        <div class="container">
+            <div class="row centered">
+                <a href="#"><i class="fa fa-vk"></i></a>
+                <a href="#"><i class="fa fa-chrome"></i></a>
+                <a href="#"><i class="fa fa-youtube"></i></a>
+                <a href="#"><i class="fa fa-github"></i></a>
+                <a href="#"><i class="fa fa-steam"></i></a>
+                <a href="#"><i class="fa fa-windows"></i></a>
+                <a href="#"><i class="fa fa-android"></i></a>
+                <a href="#"><i class="fa fa-telegram"></i></a>
+            </div>
+        </div>
+    </div>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+</body>
+</html>
